3ba2548d99b08bb0362ec5f5a793560b9500d303
Sebastian <sebastian97@gmail.com>
2025-05-01 01:19:33 -0700

    Update AggressiveAntiRemovalMixin.java

--- src/main/java/me/TreeOfSelf/PandaAntiPermanentBlockBreak/mixin/AggressiveAntiRemovalMixin.java
+++ src/main/java/me/TreeOfSelf/PandaAntiPermanentBlockBreak/mixin/AggressiveAntiRemovalMixin.java
@@ -3,6 +3,7 @@ package me.TreeOfSelf.PandaAntiPermanentBlockBreak.mixin;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
@@ -19,7 +20,7 @@ public class AggressiveAntiRemovalMixin {
             ),
             cancellable = true
     )
-    protected void onStateReplaced(BlockState state, World world, BlockPos pos, boolean moved, CallbackInfo ci) {
+    protected void onStateReplaced(BlockState state, ServerWorld world, BlockPos pos, boolean moved, CallbackInfo ci) {
        if (state.getBlock() == Blocks.BEDROCK || state.getBlock() == Blocks.END_PORTAL_FRAME || state.getBlock() == Blocks.END_PORTAL) {
            if (state.getBlock() != world.getBlockState(pos).getBlock()) {
                world.setBlockState(pos, state);
